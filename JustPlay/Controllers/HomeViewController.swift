13555ff5b8f8f1bbbbf7efd85fe5864818cf2452
Imran Razak <Imranrazak1@icloud.com>
2022-07-28 04:35:17 +0100

    Fixing CollectionViews errors.

--- JustPlay/Controllers/HomeViewController.swift
+++ JustPlay/Controllers/HomeViewController.swift
@@ -8,38 +8,44 @@
 import UIKit
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
     
     var collectionView = UICollectionView()
 
+        
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        //Layout Configruations
-        let layout =  UICollectionViewFlowLayout()
-        layout.sectionInset =  UIEdgeInsets (top: .zero, left: .zero, bottom: .zero, right: .zero)
-        layout.itemSize = CGSize(width: 164, height: 157)
         
+        //Layout Configruations
+        let layout = UICollectionViewFlowLayout()
+        //layout.sectionInset =  UIEdgeInsets (top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: 164, height: 157)
+
+    
         
         //Configuring Collection View
+        //collectionView.translatesAutoresizingMaskIntoConstraints =  false
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
         collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.Identifier)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.backgroundColor = .white
         
-        view.addSubview(collectionView)
         
+        view.addSubview(collectionView)
         
         
         
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 4
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -49,6 +55,10 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.Identifier, for: indexPath) as! CollectionViewCell
         return cell
+    }
+    
+    func configureCollectionView() {
+        
     }
 
 }
--- JustPlay/Views/CollectionViewCell.swift
+++ JustPlay/Views/CollectionViewCell.swift
@@ -11,6 +11,8 @@ class CollectionViewCell: UICollectionViewCell {
     
     static let Identifier = "CollectionViewCell"
     
+   // let genres: [String] = ["Chill", "Party", "Focus", "Sleep", "Fitness", "Feel Good", "Commuting", "Feeling Blue"]
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -18,6 +20,68 @@ class CollectionViewCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError()
     }
-   
+    
+    
+    //CollectionViewCell UI Components
+    private let ChillGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Chill"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    
+    private let FeelGoodGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Feel Good"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    
+    private let FitnessGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Fitness"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    
+    private let FeelingBlueGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Feeling Blue"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    
+    private let PartyGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Party"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    private let SleepGenre: UILabel = {
+        let genreText =  UILabel()
+        genreText.text = "Sleep Genre"
+        genreText.textColor = .systemBlue
+        return genreText
+    }()
+    
+    
+    //MARK: background Configruations
+    
+    func finishedViews() {
+        addSubview(ChillGenre)
+        addSubview(FeelGoodGenre)
+        addSubview(FitnessGenre)
+        addSubview(FeelingBlueGenre)
+        addSubview(PartyGenre)
+        addSubview(SleepGenre)
+    }
+
     
 }
+
+
